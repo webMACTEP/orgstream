b0719ddf0e3ed6ce899e6e198ae988f9349fe052
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2022-06-27 12:31:57 +0300

    ккк

--- single-blogs.php
+++ single-blogs.php
@@ -44,26 +44,13 @@ while (have_posts()) : the_post(); ?>
             <div class="nav">
                <div class="title">
                   <h3>СТАТЬИ</h3>
-                  <?php
-                  wp_nav_menu(array(
-                     'menu'            => 'blogs',              // (string) Название выводимого меню (указывается в админке при создании меню, приоритетнее 
-                     // чем указанное местоположение theme_location - если указано, то параметр theme_location игнорируется)
-                     'container'       => 'div',           // (string) Контейнер меню. Обворачиватель ul. Указывается тег контейнера (по умолчанию в тег div)
-                     'container_class' => '',              // (string) class контейнера (div тега)
-                     'container_id'    => '',              // (string) id контейнера (div тега)
-                     'menu_class'      => 'menu',          // (string) class самого меню (ul тега)
-                     'menu_id'         => '',              // (string) id самого меню (ul тега)
-                     'echo'            => true,            // (boolean) Выводить на экран или возвращать для обработки
-                     'fallback_cb'     => 'wp_page_menu',  // (string) Используемая (резервная) функция, если меню не существует (не удалось получить)
-                     'before'          => '',              // (string) Текст перед <a> каждой ссылки
-                     'after'           => '',              // (string) Текст после </a> каждой ссылки
-                     'link_before'     => '',              // (string) Текст перед анкором (текстом) ссылки
-                     'link_after'      => '',              // (string) Текст после анкора (текста) ссылки
-                     'depth'           => 0,               // (integer) Глубина вложенности (0 - неограничена, 2 - двухуровневое меню)
-                     'walker'          => '',              // (object) Класс собирающий меню. Default: new Walker_Nav_Menu
-                     'theme_location'  => ''               // (string) Расположение меню в шаблоне. (указывается ключ которым было зарегистрировано меню в функции register_nav_menus)
-                  ));
-                  ?>
+                  <ul>
+                     <?php $pc = new WP_Query('post_type=blogs'); ?>
+                     <?php while ($pc->have_posts()) : $pc->the_post(); ?>
+                        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+
+                     <?php endwhile; ?>
+                  </ul>
                </div>
             </div>
          </div>
